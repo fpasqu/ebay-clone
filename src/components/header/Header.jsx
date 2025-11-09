98c6e528a09e2598b3ef266fa0599913ea4ea4da
fpasqu <federico.pasquali99@gmail.com>
2025-11-09 21:59:04 +0100

    Init project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header() {
+  return (
+    <>
+        <div className='header'>
+            <div className='headerLeft'>
+                <p>Ciao! Accedi o registrati</p>
+                <p>eBay Extra</p>
+                <p>eBay Imperdibili</p>
+                <p>Aiuto e contatti</p>
+            </div>
+            <div className='headerRight'>
+                <p>Vendi</p>
+                <p>Il mio eBay</p>
+            </div>
+       
+        </div>
+    </>
+  )
+}
+
+export default Header
